c96dd13333d6dd1c7855356dd6eaff6075927802
joseesgu <joseesgutierrez12@gmail.com>
2026-05-02 03:19:29 -0600

    Configuración de CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"], # Agrega aquí los puertos que use tu equipo de frontend
+    allow_origins=["*"], # Agrega aquí los puertos que use tu equipo de frontend
     allow_credentials=True,
     allow_methods=["*"], # Permite GET, POST, PUT, DELETE, etc.
     allow_headers=["*"], # Permite enviar tokens de autorización y multipart/form-data
